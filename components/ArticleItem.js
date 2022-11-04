d1b45e6a7331beb5abf856e4c55262d8f42041e2
vsevolod.kulev <vsevolods.kuvevs@bertelsmann.de>
2022-11-04 13:22:43 +0200

    Mapping articles using server side props

---
+++ components/ArticleItem.js
@@ -0,0 +1,18 @@
+import React from "react"
+import articleStyles from "../styles/Article.module.css"
+import Link from "next/link"
+
+const ArticleItem = ({ article }) => {
+  return (
+    <Link
+      href="/article/[id]"
+      as={`/article/${article.id}`}
+      className={articleStyles.card}
+    >
+      <h3>{article.title} &rarr;</h3>
+      <p>{article.body}</p>
+    </Link>
+  )
+}
+
+export default ArticleItem
